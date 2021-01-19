70c640880096aefa0559103c9396539a0bd31422
Nigel Faustino <nigelfaustino@gmail.com>
2021-01-19 11:34:47 -0800

    Add content card display cell

--- BrazeSDKIntegration/ViewControllers/LoginVC.swift
+++ BrazeSDKIntegration/ViewControllers/LoginVC.swift
@@ -10,82 +10,6 @@ import Stevia
 import AppboyUI
 
 class LoginVC: UIViewController {
-//    private let loginButton: UIButton = {
-//        let button = UIButton()
-//        button.backgroundColor = .blue
-//        button.setTitle("Login", for: .normal)
-//        return button
-//    }()
-//
-//    private let idTextField: UITextField = {
-//        let textfield = UITextField()
-//        textfield.layer.borderWidth = 1
-//        textfield.placeholder = "Change user ID"
-//        textfield.layer.borderColor = UIColor.black.cgColor
-//        return textfield
-//    }()
-//
-//    private let eventTextfield: UITextField = {
-//        let textfield = UITextField()
-//        textfield.layer.borderWidth = 1
-//        textfield.placeholder = "Custom Event"
-//        textfield.layer.borderColor = UIColor.black.cgColor
-//        return textfield
-//    }()
-//
-//    private let attributeKeyTextField: UITextField = {
-//        let textfield = UITextField()
-//        textfield.layer.borderWidth = 1
-//        textfield.placeholder = "Custom Attribute Key"
-//        textfield.layer.borderColor = UIColor.black.cgColor
-//        return textfield
-//    }()
-//
-//    private let attributeValueTextField: UITextField = {
-//        let textfield = UITextField()
-//        textfield.layer.borderWidth = 1
-//        textfield.placeholder = "Custom Attribute Value"
-//        textfield.layer.borderColor = UIColor.black.cgColor
-//        return textfield
-//    }()
-//
-//    private let contentCardButton: UIButton = {
-//        let button = UIButton()
-//        button.backgroundColor = .red
-//        button.setTitle("Content Cards", for: .normal)
-//        return button
-//    }()
-//
-//    private let filterTextField: UITextField = {
-//        let textfield = UITextField()
-//        textfield.layer.borderWidth = 1
-//        textfield.placeholder = "Content Card Filter"
-//        textfield.layer.borderColor = UIColor.black.cgColor
-//        return textfield
-//    }()
-//
-//    private let customContentCardButton: UIButton = {
-//        let button = UIButton()
-//        button.backgroundColor = .gray
-//        button.setTitle("Customized Content Cards", for: .normal)
-//        return button
-//    }()
-//
-//    private let logEventButton: UIButton = {
-//        let button = UIButton()
-//        button.backgroundColor = .green
-//        button.setTitle("Log Event", for: .normal)
-//        return button
-//    }()
-//
-//    private let logAttributeButton: UIButton = {
-//        let button = UIButton()
-//        button.backgroundColor = .black
-//        button.setTitle("Log Attribute", for: .normal)
-//        return button
-//    }()
-//
-    
     private let tableView: UITableView = UITableView()
         
     override func viewDidLoad() {
@@ -95,147 +19,31 @@ class LoginVC: UIViewController {
         tableView.dataSource = self
         tableView.register(LoginCell.self, forCellReuseIdentifier: LoginCell.description())
         tableView.register(UserAnalyticsCell.self, forCellReuseIdentifier: UserAnalyticsCell.description())
+        tableView.register(ContentCardDisplayCell.self, forCellReuseIdentifier: ContentCardDisplayCell.description())
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         view.subviews(tableView)
         tableView.left(0).right(0).top(0).bottom(0)
-        
-//        view.subviews(
-//            [
-//                idTextField,
-//                loginButton,
-//                contentCardButton,
-//                eventTextfield,
-//                logEventButton,
-//                filterTextField,
-//                customContentCardButton,
-//                addUserAliasButton,
-//                userAliasTextField,
-//                userAliasLabelTextField,
-//                logAttributeButton,
-//                attributeKeyTextField,
-//                attributeValueTextField
-//            ]
-//        )
-//        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
-//        view.addGestureRecognizer(tap)
-//        layoutUI()
-//        loginButton.addTarget(self, action: #selector(loginPressed), for: .touchUpInside)
-//        contentCardButton.addTarget(self, action: #selector(contentCardPressed), for: .touchUpInside)
-//        logEventButton.addTarget(self, action: #selector(logEventButtonPressed), for: .touchUpInside)
-//        customContentCardButton.addTarget(self, action: #selector(customContentCardPressed), for: .touchUpInside)
-//        addUserAliasButton.addTarget(self, action: #selector(addAliasButtonPressed), for: .touchUpInside)
-//        logAttributeButton.addTarget(self, action: #selector(logAttributeButtonPressed), for: .touchUpInside)
     }
-    
-//PRIVATE
-    
-//    private func layoutUI() {
-//        idTextField.centerHorizontally().left(10).right(10)
-//        idTextField.Top == view.safeAreaLayoutGuide.Top + 10
-//        loginButton.Top == idTextField.Bottom + 10
-//        loginButton.centerHorizontally().left(10).right(10)
-//        contentCardButton.centerHorizontally().left(10).right(10)
-//        contentCardButton.Top == loginButton.Bottom + 10
-//        filterTextField.Top == contentCardButton.Bottom + 10
-//        filterTextField.centerHorizontally().left(10).right(10)
-//        customContentCardButton.Top == filterTextField.Bottom + 10
-//        customContentCardButton.centerHorizontally().left(10).right(10)
-//        attributeKeyTextField.Top == customContentCardButton.Bottom + 10
-//        attributeKeyTextField.left(10).right(10)
-//        attributeValueTextField.Top == attributeKeyTextField.Bottom + 10
-//        attributeValueTextField.left(10).right(10)
-//        logAttributeButton.Top == attributeValueTextField.Bottom + 10
-//        logAttributeButton.left(10).right(10)
-//        eventTextfield.centerHorizontally().left(10).right(10)
-//        eventTextfield.Top == logAttributeButton.Bottom + 10
-//        logEventButton.centerHorizontally().left(10).right(10)
-//        logEventButton.Top == eventTextfield.Bottom + 10
-//        userAliasTextField.Top == logEventButton.Bottom + 10
-//        userAliasTextField.left(10).right(10)
-//        userAliasLabelTextField.Top == userAliasTextField.Bottom + 10
-//        userAliasLabelTextField.left(10).right(10)
-//        addUserAliasButton.Top == userAliasLabelTextField.Bottom + 10
-//        addUserAliasButton.centerHorizontally().left(10).right(10)
-//    }
-//
-//    @objc private func dismissKeyboard() {
-//        view.endEditing(true)
-//    }
-//
-//    @objc private func loginPressed() {
-//        var userID = "YOUR_USER_ID"
-//        if let text = idTextField.text, !text.isEmpty {
-//            userID = text
-//            idTextField.text = ""
-//        }
-//        Appboy.sharedInstance()?.changeUser(userID)
-//        let count = Appboy.sharedInstance()?.contentCardsController.unviewedContentCardCount() ?? 0
-//        print(count)
-//        let alert = UIAlertController(title: "Changed User", message: "ID changed to \(Appboy.sharedInstance()?.user.userID ?? "No external id")", preferredStyle: .actionSheet)
-//        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
-//            NSLog("The \"OK\" alert occured.")
-//        }))
-//        present(alert, animated: true, completion: nil)
-//    }
-//
-//    @objc private func contentCardPressed() {
-//        Appboy.sharedInstance()?.requestContentCardsRefresh()
-//        let contentCards = ABKContentCardsTableViewController()
-//        contentCards.title = "Content Cards Title"
-//        contentCards.disableUnreadIndicator = true
-//        navigationController?.pushViewController(contentCards, animated: true)
-//    }
-//
-//    @objc private func customContentCardPressed() {
-//        guard let filtered = filterTextField.text else {
-//            return
-//        }
-//        Appboy.sharedInstance()?.requestContentCardsRefresh()
-//        let contentCards = CustomContentCardVC(filtered)
-//        contentCards.title = "Filtered Content Cards"
-//        navigationController?.pushViewController(contentCards, animated: true)
-//    }
-//
-//    @objc private func logAttributeButtonPressed() {
-//        guard let key = attributeKeyTextField.text, let value = attributeValueTextField.text else { return }
-//        Appboy.sharedInstance()?.user.setCustomAttributeWithKey(key, andStringValue: value)
-//        let alert = UIAlertController(title: "Set attribute", message: "Set custom attribute \(key) with value \(value)", preferredStyle: .actionSheet)
-//        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
-//            NSLog("The \"OK\" alert occured.")
-//        }))
-//        present(alert, animated: true, completion: nil)
-//        attributeKeyTextField.text = ""
-//        attributeValueTextField.text = ""
-//    }
-//
-//    @objc private func logEventButtonPressed() {
-//        guard let event = eventTextfield.text else { return }
-//        Appboy.sharedInstance()?.logCustomEvent(event)
-//        let alert = UIAlertController(title: "Logged Event", message: "Logged \(event)", preferredStyle: .actionSheet)
-//        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
-//            NSLog("The \"OK\" alert occured.")
-//        }))
-//        present(alert, animated: true, completion: nil)
-//        eventTextfield.text = ""
-//    }
-//
-//    @objc private func addAliasButtonPressed() {
-//        guard let text = userAliasTextField.text else { return }
-//        Appboy.sharedInstance()?.user.addAlias(text, withLabel: userAliasLabelTextField.text ?? "")
-//        let alert = UIAlertController(title: "Added User Alias", message: "Added \(text) as alias with label \(userAliasLabelTextField.text ?? "")", preferredStyle: .actionSheet)
-//        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
-//            NSLog("The \"OK\" alert occured.")
-//        }))
-//        present(alert, animated: true, completion: nil)
-//        userAliasTextField.text = ""
-//        userAliasLabelTextField.text = ""
-//    }
 }
 
-extension LoginVC: UITableViewDataSource, TableviewCellAlertProtocol {
+extension LoginVC: UITableViewDataSource, TableviewCellAlertProtocol, ContentCardCellDelegate {
+    func presentContentCardPressed(_ filtered: Bool, _ text: String?) {
+        Appboy.sharedInstance()?.requestContentCardsRefresh()
+        if !filtered {
+            let contentCards = ABKContentCardsTableViewController()
+            contentCards.title = "Content Cards Title"
+            contentCards.disableUnreadIndicator = true
+            navigationController?.pushViewController(contentCards, animated: true)
+        } else if let text = text {
+            let contentCards = CustomContentCardVC(text)
+            contentCards.title = "Filtered Content Cards for \(text)"
+            navigationController?.pushViewController(contentCards, animated: true)
+        }
+    }
+    
     func renderAlertViewController(_ title: String, _ message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
         alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
@@ -243,9 +51,9 @@ extension LoginVC: UITableViewDataSource, TableviewCellAlertProtocol {
         }))
         present(alert, animated: true, completion: nil)
     }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return 3
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -254,10 +62,15 @@ extension LoginVC: UITableViewDataSource, TableviewCellAlertProtocol {
                 let cell = tableView.dequeueReusableCell(withIdentifier: LoginCell.description()) as! LoginCell
                 cell.delegate = self
                 return cell
-            } else {
+            } else if indexPath.row == 1 {
                 let cell = tableView.dequeueReusableCell(withIdentifier: UserAnalyticsCell.description()) as! UserAnalyticsCell
                 cell.delegate = self
                 return cell
+            } else {
+                let cell = tableView.dequeueReusableCell(withIdentifier: ContentCardDisplayCell.description()) as! ContentCardDisplayCell
+                cell.delegate = self
+                return cell
+
             }
         }()
         return cell
